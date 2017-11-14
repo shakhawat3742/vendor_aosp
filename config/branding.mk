b7cfc12770e5e9899c854b7b89565b9deb895e9b
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-11-14 18:54:56 -0200

    vendor: Define custom build date

--- config/branding.mk
+++ config/branding.mk
@@ -1,7 +1,9 @@
 # Set all versions
-CUSTOM_VERSION := PixelExperience_$(CUSTOM_BUILD)-$(PLATFORM_VERSION)-$(shell date -u +%Y%m%d-%H%M)
+CUSTOM_BUILD_DATE := $(shell date -u +%Y%m%d-%H%M)
+CUSTOM_VERSION := PixelExperience_$(CUSTOM_BUILD)-$(PLATFORM_VERSION)-$(CUSTOM_BUILD_DATE)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     BUILD_DISPLAY_ID=$(BUILD_ID) \
-    ro.mod.version=$(CUSTOM_VERSION)
+    ro.mod.version=$(CUSTOM_VERSION) \
+    ro.mod.build_date=$(CUSTOM_BUILD_DATE)
 

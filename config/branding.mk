f39031ae6b35b959f274ba75ace2d64fc71116b5
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-12-29 15:37:48 -0200

    [1/2] vendor: branding: Rename OTA props, show build type on zip name

--- config/branding.mk
+++ config/branding.mk
@@ -1,9 +1,11 @@
 # Set all versions
+CUSTOM_BUILD_TYPE ?= UNOFFICIAL
 CUSTOM_BUILD_DATE := $(shell date -u +%Y%m%d-%H%M)
-CUSTOM_VERSION := PixelExperience_$(CUSTOM_BUILD)-$(PLATFORM_VERSION)-$(CUSTOM_BUILD_DATE)
+CUSTOM_VERSION := PixelExperience_$(CUSTOM_BUILD)-$(PLATFORM_VERSION)-$(CUSTOM_BUILD_DATE)-$(CUSTOM_BUILD_TYPE)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     BUILD_DISPLAY_ID=$(BUILD_ID) \
-    ro.mod.version=$(CUSTOM_VERSION) \
-    ro.mod.build_date=$(CUSTOM_BUILD_DATE)
+    org.pixelexperience.version=$(CUSTOM_VERSION) \
+    org.pixelexperience.build_date=$(CUSTOM_BUILD_DATE) \
+    org.pixelexperience.build_type=$(CUSTOM_BUILD_TYPE)
 

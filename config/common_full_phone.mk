49c435d38533b7b7f76bdf9a39a2a89748efbdd9
maxwen <max.weninger@gmail.com>
2018-01-03 13:07:58 +0100

    vendor: remove prebuilt spn xml file
    Change-Id: I3262797a51bc2c14dc05c878e9fd39051587462e

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -7,14 +7,6 @@ $(call inherit-product, vendor/aosp/config/common.mk)
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
-# World SPN overrides list
-PRODUCT_COPY_FILES += \
-    vendor/aosp/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
-
-# Selective SPN list for operator number who has the problem.
-PRODUCT_COPY_FILES += \
-    vendor/aosp/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
-
 # SIM Toolkit
 PRODUCT_PACKAGES += \
     Stk
